847c0725aa8c4d3b5ad1330774cb6659d58e6a61
Fabian Zierler <fabian.zierler@a1.net>
2023-12-07 11:40:31 +0000

    quark masses

--- scripts/analyse_awi_mass.jl
+++ scripts/analyse_awi_mass.jl
@@ -4,13 +4,30 @@ using Plots
 plotlyjs()
 
 path = "./output/h5files/"
-name = "Lt48Ls20beta6.5mf0.71mas1.01AS"
-file = joinpath(path,name,"out_spectrum_mixed.h5")
+names = [ 
+    "Lt48Ls20beta6.5mf0.71mas1.01AS",
+    "Lt48Ls20beta6.5mf0.71mas1.01FUN",
+    "Lt64Ls20beta6.5mf0.70mas1.01AS",
+    "Lt64Ls20beta6.5mf0.70mas1.01FUN",
+    "Lt64Ls20beta6.5mf0.71mas1.01AS",
+    "Lt64Ls20beta6.5mf0.71mas1.01FUN",
+    "Lt80Ls20beta6.5mf0.71mas1.01AS",
+    "Lt80Ls20beta6.5mf0.71mas1.01FUN",
+    "Lt96Ls20beta6.5mf0.71mas1.01AS",
+    "Lt96Ls20beta6.5mf0.71mas1.01FUN"
+]
 
-c, Δc = awi_corr(file)
+for name in names
+    file = joinpath(path,name,"out_spectrum_mixed.h5")
+    c, Δc = awi_corr(file)
 
-tmin=17
-tmax=35
-mq, Δmq = awi_fit(c,Δc;tmin,tmax)
-scatter(c,yerr=Δc)
-plot!(tmin:tmax,mq*ones(tmax-tmin+1),ribbon=Δmq)
+    T = length(c)
+
+    tmin= 15
+    tmax= T -  15
+
+    mq, Δmq = awi_fit(c,Δc;tmin,tmax)
+    plt = scatter(c,yerr=Δc)
+    plot!(plt,tmin:tmax,mq*ones(tmax-tmin+1),ribbon=Δmq)
+    display(plt)
+end
