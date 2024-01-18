eadd55b973861cfc4f419cf915e1e2ace97c80e4
Fabian Zierler <fabian.zierler@a1.net>
2024-01-18 17:21:30 +0000

    set old overall normalization

--- scripts/single_rep_with_smearing.jl
+++ scripts/single_rep_with_smearing.jl
@@ -87,9 +87,9 @@ function stdmean(X;dims)
     return m, s
 end
 
-rescale_disc = 1
-rescale_conn = false
 T,L = h5read(h5file,"lattice")[1:2]
+rescale_disc = L^3
+rescale_conn = true
 
 corrMatCONN, corrMatDISC = _smeared_correlation_matrix(h5file,Nsmear,"g5","FUN";rescale_disc,rescale_conn)
 avgMatCONN, stdMatCONN = stdmean(corrMatCONN;dims=3)
@@ -97,7 +97,7 @@ avgMatDISC, stdMatDISC = stdmean(corrMatDISC;dims=3)
 
 # add data from publication as reference
 conn_old = h5read(h5data,joinpath(group,file1,channel1))
-MixedRepSinglets.rescale_connected!(conn_old,1)
+MixedRepSinglets.rescale_connected!(conn_old,L)
 corr_conn, corr_conn_Delta = stdmean(conn_old;dims=2)
 
 disc_old = h5read(h5data,joinpath(group,file2,channel2))
@@ -108,19 +108,20 @@ corr_disc, corr_disc_Delta = stdmean(corr_disc_old,dims=1)
 discFUN_noAPE = _get_disconnected_at_smearing_level(h5file_noAPE,0,"g5","FUN")
 connFUN_noAPE = _get_connected_at_smearing_level(h5file_noAPE,0,0,"g5","FUN") 
 discFUN_corr_noAPE = unbiased_estimator(discFUN_noAPE;rescale=rescale_disc)
+MixedRepSinglets.rescale_connected!(connFUN_noAPE,L)
 corr_disc_noAPE, corr_disc_noAPE_Delta = stdmean(discFUN_corr_noAPE,dims=1)
 corr_conn_noAPE, corr_conn_noAPE_Delta = stdmean(connFUN_noAPE,dims=1)
 
 plt1 = plot()
 plt2 = plot()
 for i in 1:3
-    scatter!(plt1,0.5avgMatCONN[i,i,:],yerr=0.5stdMatCONN[i,i,:],label="N=$(Nsmear[i]) (with APE)(conn.)")
+    scatter!(plt1,avgMatCONN[i,i,:],yerr=stdMatCONN[i,i,:],label="N=$(Nsmear[i]) (with APE)(conn.)")
     scatter!(plt2,2avgMatDISC[i,i,:],yerr=2stdMatDISC[i,i,:],label="N=$(Nsmear[i]) (with APE)(disc.)")
 end
 plt1
 scatter!(plt1,corr_conn,yerr=corr_conn_Delta, label="no APE no Wuppertal (old)")
 scatter!(plt2,corr_disc,yerr=corr_disc_Delta, label="no APE no Wuppertal (old)")
-scatter!(plt1,0.5corr_conn_noAPE,yerr=0.5corr_conn_noAPE_Delta, label="no APE no Wuppertal")
+scatter!(plt1,corr_conn_noAPE,yerr=corr_conn_noAPE_Delta, label="no APE no Wuppertal")
 scatter!(plt2,4corr_disc_noAPE,yerr=4corr_disc_noAPE_Delta, label="no APE no Wuppertal")
-plot!(plt1,yscale=:log10)
 plot!(plt2,yscale=:log10)
+plot!(plt1,yscale=:log10)
