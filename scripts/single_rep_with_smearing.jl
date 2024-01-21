7ecb146c0595f8768a6c6c14d548a40fd37be13b
Fabian Zierler <fabian.zierler@a1.net>
2024-01-21 16:21:42 +0000

    cosmetic changes

--- scripts/single_rep_with_smearing.jl
+++ scripts/single_rep_with_smearing.jl
@@ -92,39 +92,39 @@ rescale_disc = L^3
 rescale_conn = true
 Nf=2
 
+# read data with and without APE smearing
 conn_matrix, disc_matrix = _smeared_singlet_correlation_matrix(h5file,Nsmear,"g5","FUN";rescale_disc,rescale_conn)
 conn_noAPE,  disc_noAPE  = _smeared_singlet_correlator(h5file_noAPE,0,"g5","FUN";rescale=rescale_disc)
 
 # add data from publication as reference
 h5data = "/home/fabian/Downloads/data.hdf5"
-group  = "runsSp4/Lt24Ls12beta6.9m1-0.90m2-0.90"
-file1  = "out_spectrum"
-file2  = "out_spectrum_discon"
-channel1 = "DEFAULT_SEMWALL TRIPLET_g5"
-channel2 = "DISCON_SEMWALL SINGLET_g5_disc_re"
-
-conn_old = h5read(h5data,joinpath(group,file1,channel1))
-disc_old = h5read(h5data,joinpath(group,file2,channel2))
+data_conn = "runsSp4/Lt24Ls12beta6.9m1-0.90m2-0.90/out_spectrum/DEFAULT_SEMWALL TRIPLET_g5"
+data_disc = "runsSp4/Lt24Ls12beta6.9m1-0.90m2-0.90/out_spectrum_discon/DISCON_SEMWALL SINGLET_g5_disc_re"
+conn_old = h5read(h5data,data_conn)
+loop_old = h5read(h5data,data_disc)
+disc_old = unbiased_estimator(loop_old;rescale=rescale_disc)
 MixedRepSinglets.rescale_connected!(conn_old,L)
-corr_disc_old = unbiased_estimator(disc_old;rescale=rescale_disc)
-corr_conn, corr_conn_Delta = stdmean(conn_old;dims=2)
-corr_disc, corr_disc_Delta = stdmean(corr_disc_old,dims=1)
 
 # built full singlet correlator
 # PART 1: Smeared correlators
 #         Factor 2 is missing relative factor (not the Nf factor)
 # PART 2: Smeared correlators without APE smearing (Nf included)
 # PART 3: Old published data 
-corr1 = conn_matrix   - 4Nf * disc_matrix
-corr2 = conn_noAPE    - 4Nf * disc_noAPE
-corr3 = conn_old'     -  Nf * corr_disc_old
+corr1 = conn_matrix - 4Nf * disc_matrix
+corr2 = conn_noAPE  - 4Nf * disc_noAPE
+corr3 = conn_old'   -  Nf * corr_disc_old
+
+# Perform GEVP
+samples = eigenvalues_jackknife_samples(corr1)
+m, Δm = meff_from_jackknife(samples;sign=+1)
+
 # permute dimensions so that the first index corresponds to Euclidean time
-#                            the second index refers to the Monte-Carlo time
+# and the second index refers to the Monte-Carlo time
 corr1 = permutedims(corr1,(4,3,1,2))
 corr2 = permutedims(corr2,(2,1))
 corr3 = permutedims(corr3,(2,1))
 
-c1, Δc1 = stdmean(corr1,dims=2)
+c1, Δc1 = stdmean(corr1,dims=3)
 c2, Δc2 = stdmean(corr2,dims=2) 
 c3, Δc3 = stdmean(corr3,dims=2)
 
@@ -133,21 +133,19 @@ meff1, Δmeff1 = implicit_meff_jackknife(corr1;sign=+1)
 meff2, Δmeff2 = implicit_meff_jackknife(corr2;sign=+1)
 meff3, Δmeff3 = implicit_meff_jackknife(corr3;sign=+1)
 
-# Perform GEVP
-# transform to the old inidces
-corr1 = permutedims(corr1,(3,4,2,1))
-jks = eigenvalues_jackknife_samples(corr1)
-m, Δm = meff_from_jackknife(jks;sign=+1)
-
 plt1 = plot()
+plt2 = plot()
+scatter!(plt2,m[3,:],yerr=Δm[3,:], label="GEVP N=0,10,20 (with APE)")
 for i in 1:3
-    scatter!(plt1,c1[i,i,:],yerr=Δc1[i,i,:],label="N=$(Nsmear[i]) (with APE)(conn.)")
+    scatter!(plt1,c1[i,i,:]    ,yerr=Δc1[i,i,:]   ,label="N=$(Nsmear[i]) (with APE)")
+    #scatter!(plt2,meff1[:,i,i] ,yerr=Δmeff1[:,i,i],label="N=$(Nsmear[i]) (with APE)")
 end
-scatter!(plt1,c2,yerr=Δc2, label="no APE no Wuppertal (old)")
-scatter!(plt1,c3,yerr=Δc3, label="no APE no Wuppertal")
+scatter!(plt1,c3,yerr=Δc3, label="N=0 (no APE)")
+scatter!(plt1,c2,yerr=Δc2, label="N=0 (no APE) (old)")
+#scatter!(plt2,meff3 ,yerr=Δmeff3, label="N=0 (no APE)")
+scatter!(plt2,meff2 ,yerr=Δmeff2, label="N=0 (no APE) (old)")
 plot!(plt1,yscale=:log10)
+plot!(plt2,xlims=(0,12),ylims=(0.3,1.1))
 
-plt3 = plot()
-scatter!(plt3,meff3 ,yerr=Δmeff3)
-scatter!(plt3,m[3,:],yerr=Δm[3,:])
-plot!(xlims=(0,12),ylims=(0.3,1.1))
+display(plt1)
+display(plt2)
