acf2b3436c341008a0b74a0a1f6a1ed338364bd0
Fabian Zierler <fabian.zierler@a1.net>
2024-03-08 16:41:14 +0000

    fix comparison plot for single representation data

--- scripts/with_smearing_single_rep.jl
+++ scripts/with_smearing_single_rep.jl
@@ -1,6 +1,7 @@
 using Pkg; Pkg.activate(".")
 using MixedRepSinglets
 using Plots
+using HDF5
 gr(fontfamily="Computer Modern",  top_margin=4Plots.mm, left_margin=4Plots.mm, legend=:topright, frame=:box, legendfontsize=11, tickfontsize=10, labelfontsize=14, markersize=6)
 
 h5file = "/home/fabian/Downloads/single_rep_smeared.hdf5"
@@ -25,58 +26,43 @@ disc_old = Nf*unbiased_estimator(loop_old;subtract_vev = false, rescale=L^3)
 MixedRepSinglets.rescale_connected!(conn_old,L)
 
 # built full singlet correlator
-corr1 = conn_matrix - disc_matrix
-corr3 = conn_old'   - disc_old
+corr_new = conn_matrix - disc_matrix
+corr_old = conn_old'   - disc_old
 
-corr1 = correlator_folding(corr1;t_dim=4,sign)
-corr3 = correlator_folding(corr3;t_dim=2,sign)
-conn  = correlator_folding(conn_matrix;t_dim=2,sign)
-disc  = correlator_folding(disc_matrix;t_dim=2,sign)
+corr_new = correlator_folding(corr_new;t_dim=4,sign)
+corr_old = correlator_folding(corr_old;t_dim=2,sign)
+
+c1, Δc1 = stdmean(corr_new,dims=3)
+c3, Δc3 = stdmean(corr_old,dims=1)
 
 # symmetry sign of correlators
-#corr1 = correlator_derivative(corr1;t_dim=4)
-#corr3 = correlator_derivative(corr3;t_dim=2)
-#sign = -1
+corr_new = correlator_derivative(corr_new;t_dim=4)
+corr_old = correlator_derivative(corr_old;t_dim=2)
+sign = -1
 
-# permute dimensions so that the first index corresponds to Euclidean time
-# and the second index refers to the Monte-Carlo time
-corr1 = permutedims(corr1,(4,3,1,2))
-disc  = permutedims(disc ,(4,3,1,2))
-conn  = permutedims(conn ,(4,3,1,2))
-corr3 = permutedims(corr3,(2,1))
+corr_new
+corr_old
 
-c1, Δc1 = stdmean(corr1,dims=2)
-c3, Δc3 = stdmean(corr3,dims=2)
-c3C, Δc3C = stdmean(conn_old,dims=2)
-c3D, Δc3D = stdmean(disc_old,dims=2)
-cD, ΔcD = stdmean(disc,dims=2)
-cC, ΔcC = stdmean(conn,dims=2)
+samples = eigenvalues_jackknife_samples(corr_new,t0=1)
+meff_new, Δmeff_new = meff_from_jackknife(samples;sign)
+meff_old, Δmeff_old = implicit_meff_jackknife(corr_old';sign)
+meff_APE, Δmeff_APE = implicit_meff_jackknife(corr_new[1,1,:,:]';sign)
 
-plt1 = plot()
-scatter!(plt1,cC[:,1,1],yerr=ΔcC[:,1,1],yscale=:log10)
-scatter!(plt1,cC[:,2,2],yerr=ΔcC[:,2,2],yscale=:log10)
-scatter!(plt1,cD[:,1,1],yerr=ΔcD[:,1,1],yscale=:log10)
-scatter!(plt1,cD[:,2,2],yerr=ΔcD[:,2,2],yscale=:log10)
-#scatter!(plt1,c3       ,yerr=Δc3       ,yscale=:log10)
-#scatter!(plt1,c1[:,1,1],yerr=Δc1[:,1,1],yscale=:log10)
+# Plot correlator data without drivative
+plt1 = plot(yscale=:log10)
+scatter!(plt1,c3       ,yerr=Δc3       , label ="N=0 (with APE)")
+scatter!(plt1,c1[1,1,:],yerr=Δc1[1,1,:], label ="published data")
 display(plt1)
 
 # Plot effective mass
-corr1 = permutedims(corr1,(4,3,1,2))
-corr1
-samples = eigenvalues_jackknife_samples(corr1,t0=1)
-m, Δm = meff_from_jackknife(samples;sign)
-meff1, Δmeff1 = implicit_meff_jackknife(corr1;sign)
-meff3, Δmeff3 = implicit_meff_jackknife(corr3;sign)
-
+range  = 2:10 
 xlabel = "Euclidean time"
 ylabel = "effective mass"
 title  = "pseudoscalar singlet: with numerical derivative"
-
 plt2 = plot(;xlabel,ylabel,title)
-plot!(plt2,xlims=(0,12),ylims=(0.4,0.9))
-scatter!(plt2,m[Ns,1:10],yerr=Δm[Ns,1:10], label="GEVP N=0...$N_max (with APE)")
-scatter!(meff1[:,1,1], yerr=Δmeff1[:,1,1], label="")
-scatter!(meff1[:,2,2], yerr=Δmeff1[:,2,2], label="")
+plot!(plt2,xlims=(0,12),ylims=(0.5,0.9))
+scatter!(plt2,range, meff_new[Ns,range],yerr=Δmeff_new[Ns,range], label="GEVP N=0...$N_max (with APE)")
+scatter!(plt2,range, meff_old[range,],yerr=Δmeff_old[range,], label="no smearing")
+scatter!(plt2,range, meff_APE[range,],yerr=Δmeff_APE[range,], label="only APE")
 hspan!(plt2,[0.604,0.616],alpha=0.5,label="published result")
 display(plt2)
