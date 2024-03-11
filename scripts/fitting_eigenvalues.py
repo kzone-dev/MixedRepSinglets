f41ead9f5cc760819ba1e33761849367489c3833
Fabian Zierler <fabian.zierler@a1.net>
2024-03-11 11:16:09 +0000

    fix fitting in corrfitter

--- scripts/fitting_eigenvalues.py
+++ scripts/fitting_eigenvalues.py
@@ -9,14 +9,14 @@ def get_hdf5_value(hdf5file,key):
 
 def make_models(T,tmin,tmax):
     """ Create corrfitter model for G(t). """
-    return [cf.Corr2(datatag='Gab', tp=-T, tmin=tmin, tmax=tmax, a='a', b='a', dE='dE')]
+    return [cf.Corr2(datatag='Gab', tp=None, tmin=tmin, tmax=tmax, a='a', b='a', dE='dE')]
 
 def make_prior(N):
     prior = gv.BufferDict()
     # NOTE: use a log-Gaussion distrubtion for forcing positive energies
     # NOTE: Even with this code they can be recovered by providing loose priors of 0.1(1) for both
-    prior['log(a)']  = gv.log(gv.gvar(N * ['0.1(0.1)']))
-    prior['log(dE)'] = gv.log(gv.gvar(N * ['0.5(0.3)']))
+    prior['log(a)']  = gv.log(gv.gvar(N * ['1(1)']))
+    prior['log(dE)'] = gv.log(gv.gvar(N * ['1(1)']))
     return prior
 
 def first_fit_parameters(fit):
@@ -38,7 +38,7 @@ def fit_correlator_without_bootstrap(avg,T,tmin,tmax,Nmax,plotting=False,printin
     fitter = cf.CorrFitter(models=make_models(T,tmin,tmax))
     p0 = None
     # TODO: find good Nmax
-    for N in range(2,Nmax+1):
+    for N in range(1,Nmax+1):
         prior = make_prior(N)
         fit = fitter.lsqfit(data=avg, prior=prior, p0=p0)
         p0 = fit.pmean
@@ -54,22 +54,22 @@ def fit_correlator_without_bootstrap(avg,T,tmin,tmax,Nmax,plotting=False,printin
         fit.show_plots(view='log'  )
     return E, a, chi2, dof
 
-def fit_eigenvalues_file(hdf5file,tmin,tmax1,tmax2,Nmax=10):
+def fit_eigenvalues_file(hdf5file,tmin1,tmin2,tmax1,tmax2,Nmax=10,ensemble="M1"):
     f = h5py.File(hdf5file)
-    T = get_hdf5_value(f,"M4/lattice")[0]
-    L = get_hdf5_value(f,"M4/lattice")[1]
+    T = get_hdf5_value(f,ensemble+"/lattice")[0]
+    L = get_hdf5_value(f,ensemble+"/lattice")[1]
 
-    ev = get_hdf5_value(f,"M4/eigvals_g5_singlet")[()]
-    Delta_ev = get_hdf5_value(f,"M4/Delta_eigvals_g5_singlet")[()]
+    ev = get_hdf5_value(f,ensemble+"/eigvals_g5_singlet")[()]
+    Delta_ev = get_hdf5_value(f,ensemble+"/Delta_eigvals_g5_singlet")[()]
 
     eig1 = dict(Gab=gv.gvar(ev[:,17],Delta_ev[:,17]))
     eig2 = dict(Gab=gv.gvar(ev[:,16],Delta_ev[:,16]))
 
-    E1, a1, chi2A, dofA = fit_correlator_without_bootstrap(eig1,T,tmin,tmax1,Nmax,plotting=PLOT,printing=PRINT)
-    E2, a2, chi2B, dofB = fit_correlator_without_bootstrap(eig2,T,tmin,tmax2,Nmax,plotting=PLOT,printing=PRINT)
+    E1, a1, chi2A, dofA = fit_correlator_without_bootstrap(eig1,T,tmin1,tmax1,Nmax,plotting=PLOT,printing=PRINT)
+    E2, a2, chi2B, dofB = fit_correlator_without_bootstrap(eig2,T,tmin2,tmax2,Nmax,plotting=PLOT,printing=PRINT)
     
-    beta = get_hdf5_value(f,"M4/beta")
-    mass = get_hdf5_value(f,"M4/quarkmasses_fundamental")[0]
+    beta = get_hdf5_value(f,ensemble+"/beta")
+    mass = get_hdf5_value(f,ensemble+"/quarkmasses_fundamental")[0]
 
     print("T,L,m0,beta,m_meson,chi2/dof")
     print( T,",",L,",",mass,",",beta,",",E1[0],",",chi2A/dofA)
@@ -79,4 +79,7 @@ PLOT=False
 PRINT=False
 
 filename="/home/fabian/Downloads/smeared_singlet_eigenvalues_M1234.hdf5"
-fit_eigenvalues_file(filename,3,10,10,2)
+fit_eigenvalues_file(filename,tmin1=3,tmin2=3,tmax1=10,tmax2=7,Nmax=4,ensemble="M1")
+fit_eigenvalues_file(filename,tmin1=3,tmin2=3,tmax1=10,tmax2=9,Nmax=4,ensemble="M2")
+fit_eigenvalues_file(filename,tmin1=3,tmin2=3,tmax1=10,tmax2=7,Nmax=4,ensemble="M3")
+fit_eigenvalues_file(filename,tmin1=3,tmin2=3,tmax1=10,tmax2=8,Nmax=4,ensemble="M4")
