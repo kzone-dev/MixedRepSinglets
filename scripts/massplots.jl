8530ad35a0f163e58033cf305d3692939c9d200e
Fabian Zierler <fabian.zierler@a1.net>
2024-03-12 16:59:17 +0000

    improve effective mass plots

--- scripts/massplots.jl
+++ scripts/massplots.jl
@@ -1,10 +1,19 @@
 function all_effective_mass_plots(h5eigenvals,gevp_parameterfile)
 
+    function channel_tags(channel)
+        isequal(channel,"g5_singlet")        && return L"$J^P = 0^-$(singlet)"
+        isequal(channel,"g5_nonsinglet_FUN") && return L"$J^P = 0^-$(f)"
+        isequal(channel,"g5_nonsinglet_AS")  && return L"$J^P = 0^-$(as)"
+        isequal(channel,"g1_nonsinglet_FUN") && return L"$J^P = 1^+$(f)"
+        isequal(channel,"g1_nonsinglet_AS")  && return L"$J^P = 1^+$(as)"
+        return ""
+    end
+
     parameters = readdlm(gevp_parameterfile,';';skipstart=1)
     nrows = first(size(parameters))
     for row in 1:nrows
 
-        row > 4 && continue
+        #row > 4 && continue
 
         ensemble, channel, t0, binsize, deriv, ops = parameters[row,:]
         
@@ -19,11 +28,10 @@ function all_effective_mass_plots(h5eigenvals,gevp_parameterfile)
         Δmeff    = h5read(h5eigenvals,joinpath(ensemble,channel,"Delta_meff"))
         Δeigvals = h5read(h5eigenvals,joinpath(ensemble,channel,"Delta_eigvals"))
 
-        plt1, plt2 = _plot_meff_eigvals(meff,Δmeff,eigvals,Δeigvals,β,T,L,mf,mas;nstates=2,tag=L"$J^P = 0^-$(singlet)")
+        plt1, plt2 = _plot_meff_eigvals(meff,Δmeff,eigvals,Δeigvals,β,T,L,mf,mas;nstates=2,tag=channel_tags(channel))
         plot!(plt1, ylims=(0.3,1.2))
         plot!(plt2,yscale=:log10)
         display(plt1)
-        display(plt2)
+        #display(plt2)
     end
 end
-
