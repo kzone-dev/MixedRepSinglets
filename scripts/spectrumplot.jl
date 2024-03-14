93c2b5c28bd79984659e2228f87f6bff06ffb87e
Fabian Zierler <fabian.zierler@a1.net>
2024-03-14 14:03:06 +0000

    mehr plots

--- scripts/spectrumplot.jl
+++ scripts/spectrumplot.jl
@@ -1,21 +1,26 @@
-function plot_spectrum(tablepath)
+function plot_spectrum(tablepath,gradient_flow_results="input/gradient_flow_results.csv")
     results_MR = joinpath(tablepath,"table_results_MR.csv")
 
     # get meson masses
     data = readdlm(results_MR,';',skipstart=1)
+    gf = readdlm(gradient_flow_results,',',skipstart=1)
 
     plt0 = plot(legend=:right,ylabel=L"meson masses $[a^{-1}]$", xlabel=L"$m_0^{\rm f}$", title="meson masses vs. bare fermion mass")
     plt1 = plot(legend=:right,ylabel=L"meson masses $[a^{-1}]$", xlabel=L"N_t", title=L"meson masses vs. spatial extent $N_t$")
-    plt2 = plot(legend=:right,ylabel=L"meson masses $[a^{-1}]$", xlabel=L"m_\pi / m_\rho", title = L"meson masses vs. $m_\pi / m_\rho ~ (N_t=64)$" )
-    #plt3 = plot(legend=:right,ylabel=L"meson masses $[m_\pi^{(f)}]$", xlabel=L"m_\pi / m_\rho", title=L"meson mass ratios $(N_t=64)$")
+    plt2 = plot(legend=:right,ylabel=L"meson masses $[a^{-1}]$", xlabel=L"m_\pi^{\rm f} / m_\rho^{\rm f}", title = L"meson masses vs. fundamental $m_\pi / m_\rho ~ (N_t=64)$" )
+    plt3 = plot(legend=:right,ylabel=L"meson masses $[w]$", xlabel=L"m_\pi^{\rm f} / m_\rho^{\rm f}", title=L"meson mass in gradient flow scale $w ~ (N_t=64)$")
     
     xticks0 = Float64[]
     xticks1 = Int[]
 
-    for row in eachrow(data[:,3:18])
-        T, L, mf, mas, ma, Δma, mη, Δmη, mπF, ΔmπF, mπA, ΔmπA, mρF, ΔmρF, mρA, ΔmρA = row
+    for row in eachrow(data)
+        ensemble, channel, T, L, mf, mas, ma, Δma, mη, Δmη, mπF, ΔmπF, mπA, ΔmπA, mρF, ΔmρF, mρA, ΔmρA = row
+
+        ensemble_id = findfirst(isequal(ensemble), gf[:,1])
+        w, Δw = gf[ensemble_id,7], gf[ensemble_id,8]
 
         Δratio(x,y,Δx,Δy) = sqrt((Δx/y)^2 + (x*Δy/y^2)^2) 
+        Δproduct(x,y,Δx,Δy) = sqrt((Δx*y)^2 + (x*Δy)^2)
         r  = mπF/mρF
         Δr = Δratio(mπF,mρF,ΔmπF,ΔmρF)
 
@@ -52,11 +57,20 @@ function plot_spectrum(tablepath)
             scatter!(plt2, [r], xerr = Δr, [mπA], yerr=ΔmπA,label="", marker=:circ, color=:blue)
             scatter!(plt2, [r], xerr = Δr, [mρF], yerr=ΔmρF,label="", marker=:pentagon, color=:black)
             scatter!(plt2, [r], xerr = Δr, [mρA], yerr=ΔmρA,label="", marker=:pentagon, color=:black)
+
+            scatter!(plt3, [r], xerr = Δr, [w*ma],  yerr=Δproduct(ma,w,Δma,Δw),label="", marker=:rect, color=:red)
+            scatter!(plt3, [r], xerr = Δr, [w*mη],  yerr=Δproduct(mη,w,Δmη,Δw),label="", marker=:rect, color=:red)
+            scatter!(plt3, [r], xerr = Δr, [w*mπF], yerr=Δproduct(mπF,w,ΔmπF,Δw),label="", marker=:circ, color=:blue)
+            scatter!(plt3, [r], xerr = Δr, [w*mπA], yerr=Δproduct(mπA,w,ΔmπA,Δw),label="", marker=:circ, color=:blue)
+            scatter!(plt3, [r], xerr = Δr, [w*mρF], yerr=Δproduct(mρF,w,ΔmρF,Δw),label="", marker=:pentagon, color=:black)
+            scatter!(plt3, [r], xerr = Δr, [w*mρA], yerr=Δproduct(mρA,w,ΔmρA,Δw),label="", marker=:pentagon, color=:black)
+
         end
         plot!(plt2,xlims=(0.88,0.92))
+        plot!(plt3,xlims=(0.88,0.92))
     end
 
-    for plt in (plt1,plt2,plt0)
+    for plt in (plt0,plt1,plt2,plt3)
         scatter!(plt, [],[],label=L"$J^P = 0^-$ (singlet)",marker=:rect, color=:red)
         scatter!(plt, [],[],label=L"$J^P = 0^-$ (nonsinglet)",marker=:circ, color=:blue)
         scatter!(plt, [],[],label=L"$J^P = 1^+$ (nonsinglet)",marker=:pentagon, color=:black)
@@ -65,5 +79,6 @@ function plot_spectrum(tablepath)
     display(plt0)
     display(plt1)
     display(plt2)
+    display(plt3)
     return plt0, plt1, plt2
 end
