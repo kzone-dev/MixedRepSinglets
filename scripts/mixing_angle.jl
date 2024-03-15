125d524611e86305cc5de09e860d1264cd93e04c
Fabian Zierler <fabian.zierler@a1.net>
2024-03-15 19:22:57 +0000

    start setting up the effective mixing angle

--- scripts/mixing_angle.jl
+++ scripts/mixing_angle.jl
@@ -12,13 +12,16 @@ asin_deriv(x) = +1/sqrt(1-x^2)
 acos_deriv(x) = -1/sqrt(1-x^2)
 asin_error(x,Δx) = abs(asin_deriv(x))*Δx
 acos_error(x,Δx) = abs(acos_deriv(x))*Δx
-function plot_effective_mixing_angle(evecs, Δevecs)
+function plot_eigenvectors(evecs, Δevecs)
     plt = plot(legend=:outerright)
     scatter!(plt, evecs[1,1,:],  ms = 8, yerr = Δevecs[1,1,:],label="(1,1)")
     scatter!(plt, evecs[1,2,:],  ms = 8, yerr = Δevecs[1,2,:],label="(1,2)")
     scatter!(plt, evecs[2,1,:],  ms = 5, yerr = Δevecs[2,1,:],label="(2,1)")
     scatter!(plt, evecs[2,2,:],  ms = 5, yerr = Δevecs[2,2,:],label="(2,2)")
     return plt
+end
+function plot_effective_mixing_angle(evecs_jackknife)
+
 end
 
 parameters_gevp = joinpath("input","parameters_gevp.csv")
