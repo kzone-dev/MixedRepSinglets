ec8b1076c0329792121224c814dadbf6207b4d59
Fabian Zierler <fabian.zierler@a1.net>
2024-04-15 15:31:23 +0100

    Paul's parameters (all F, one AS)

--- main.jl
+++ main.jl
@@ -19,14 +19,14 @@ include("scripts/tex_tables.jl")
 include("scripts/mixing_angle.jl")
 gr(fontfamily="Computer Modern",  top_margin=4Plots.mm, left_margin=4Plots.mm, legend=:topright, frame=:box, legendfontsize=12, tickfontsize=12, labelfontsize=14, titlefontsize=14,  markersize=5)
 
-start_from_logs    = false
-write_correlator   = false
+start_from_logs    = true
+write_correlator   = true
 write_gevp_results = true
 
 Nsmear = collect(0:10:80)
 
 logpath        = "/home/fabian/Documents/DataDiaL/measurements"
-hdf5path       = "/home/fabian/Downloads/hdf5out"
+hdf5path       = "/home/fabian/Downloads/hdf5out_new"
 paramter_path  = "input"
 corrfitterpath = "output/fitresults"
 tablepath      = "output/tables"
