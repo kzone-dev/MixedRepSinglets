7b31fa08a409a366cb4f56348c958c7ef5d448af
Fabian Zierler <fabian.zierler@a1.net>
2024-05-04 09:10:59 +0100

    start fitting (not working)

--- scripts/fitting_eigenvalues.py
+++ scripts/fitting_eigenvalues.py
@@ -49,112 +49,46 @@ def fit_correlator_without_bootstrap(avg,T,tmin,tmax,Nmax,tp,plotting=False,prin
         fit.show_plots(view='log'  )
     return E, a, chi2, dof
 
-def fit_eigenvalues(outfile,outfileHR,hdf5file,tmin1,tmin2,tmax1,tmax2,tp,Nmax,ensemble,channel,header=False):
+def fit_eigenvalues(outfile,outfileHR,hdf5file,tmin1,tmax1,tp,Nmax,ensemble,channel,rep,header=False):
     f = h5py.File(hdf5file)
-    T = get_hdf5_value(f,ensemble+"/"+channel+"/lattice")[0]
-    L = get_hdf5_value(f,ensemble+"/"+channel+"/lattice")[1]
+    T = get_hdf5_value(f,ensemble+"/"+rep+"/CONN/lattice")[0]
+    L = get_hdf5_value(f,ensemble+"/"+rep+"/CONN/lattice")[1]
     tp = tp*T if tp != 0 else None
 
-    ev = get_hdf5_value(f,ensemble+"/"+channel+"/eigvals")[()]
-    cov_ev = get_hdf5_value(f,ensemble+"/"+channel+"/eigvals_cov")[()]
-    Delta_ev = get_hdf5_value(f,ensemble+"/"+channel+"/Delta_eigvals")[()]
-
-    Nops = ev.shape[1]
-    eig1 = dict(Gab=gv.gvar(ev[:,Nops-1],cov_ev[:,:,Nops-1]))
-    eig2 = dict(Gab=gv.gvar(ev[:,Nops-2],cov_ev[:,:,Nops-2]))
-
+    h5name = ensemble+"/"+rep+"/CONN/DEFAULT_SEMWALL TRIPLET/"+channel
+    print(h5name)
+    corr = get_hdf5_value(f,h5name)[()]
+    
+    dset = gv.dataset.avg_data(corr)
+    eig1 = dict(Gab=dset)
+    
     E1, a1, chi2A, dofA = fit_correlator_without_bootstrap(eig1,T,tmin1,tmax1,Nmax,tp,plotting=PLOT,printing=PRINT)
-    E2, a2, chi2B, dofB = fit_correlator_without_bootstrap(eig2,T,tmin2,tmax2,Nmax,tp,plotting=PLOT,printing=PRINT)
     
-    beta = get_hdf5_value(f,ensemble+"/"+channel+"/beta")
-    mf   = get_hdf5_value(f,ensemble+"/"+channel+"/quarkmasses_fundamental")[0]
-    mas  = get_hdf5_value(f,ensemble+"/"+channel+"/quarkmasses_antisymmetric")[0]
+    beta = get_hdf5_value(f,ensemble+"/"+rep+"/CONN/beta")
 
     out = open(outfile, "a")
     outHR = open(outfileHR, "a")
-    out.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,mf,mas,beta,gv.mean(E1[0]),gv.sdev(E1[0]),gv.mean(E2[0]),gv.sdev(E2[0]),chi2A/dofA,chi2B/dofB))
-    outHR.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,mf,mas,beta,E1[0],E2[0],chi2A/dofA,chi2B/dofB))
+    out.write("%s;%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,beta,gv.mean(E1[0]),gv.sdev(E1[0]),chi2A/dofA))
+    outHR.write("%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,beta,E1[0],chi2A/dofA))
     out.close()
     outHR.close()
 
-def fit_eigenvalues_resample(outfile,outfileHR,hdf5file,tmin1,tmin2,tmax1,tmax2,tp,Nmax,ensemble,channel,header=False):
-    f = h5py.File(hdf5file)
-    T = get_hdf5_value(f,ensemble+"/"+channel+"/lattice")[0]
-    L = get_hdf5_value(f,ensemble+"/"+channel+"/lattice")[1]
-    tp = tp*T if tp != 0 else None
 
-    Delta_ev = get_hdf5_value(f,ensemble+"/"+channel+"/Delta_eigvals")[()]
-    cov_ev   = get_hdf5_value(f,ensemble+"/"+channel+"/eigvals_cov")[()]
-    ev_resamples = get_hdf5_value(f,ensemble+"/"+channel+"/eigvals_resamples")[()]
-    
-    T, nsamples, Nops = ev_resamples.shape
-    print(T, nsamples, Nops)
-
-    E1_samples = np.zeros(nsamples)
-    E2_samples = np.zeros(nsamples)
-    a1_samples = np.zeros(nsamples)
-    a2_samples = np.zeros(nsamples)
-    chi2dofA_samples = np.zeros(nsamples)
-    chi2dofB_samples = np.zeros(nsamples)
-
-    for n in range(nsamples):
-
-        eig1 = dict(Gab=gv.gvar(ev_resamples[:,n,Nops-1],cov_ev[:,:,Nops-1]))
-        eig2 = dict(Gab=gv.gvar(ev_resamples[:,n,Nops-2],cov_ev[:,:,Nops-2]))
-
-        E1, a1, chi2A, dofA = fit_correlator_without_bootstrap(eig1,T,tmin1,tmax1,Nmax,tp,plotting=False,printing=False)
-        E2, a2, chi2B, dofB = fit_correlator_without_bootstrap(eig2,T,tmin2,tmax2,Nmax,tp,plotting=False,printing=False)
-
-        E1_samples[n] = gv.mean(E1[0]) 
-        E2_samples[n] = gv.mean(E2[0])
-        a1_samples[n] = gv.mean(a1[0])
-        a2_samples[n] = gv.mean(a2[0])
-        chi2dofA_samples[n] = chi2A/dofA 
-        chi2dofB_samples[n] = chi2B/dofB
-
-    # this dataset uses jackknife resampling
-    E1 = gv.gvar(np.mean(E1_samples), np.sqrt(nsamples)*np.std(E1_samples,ddof=1))
-    E2 = gv.gvar(np.mean(E2_samples), np.sqrt(nsamples)*np.std(E2_samples,ddof=1))
-    a1 = gv.gvar(np.mean(a1_samples), np.sqrt(nsamples)*np.std(a1_samples,ddof=1))
-    a2 = gv.gvar(np.mean(a2_samples), np.sqrt(nsamples)*np.std(a2_samples,ddof=1))
-    chi2dofA = np.mean(chi2dofA_samples)
-    chi2dofB = np.mean(chi2dofB_samples)
-
-    beta = get_hdf5_value(f,ensemble+"/"+channel+"/beta")
-    mf   = get_hdf5_value(f,ensemble+"/"+channel+"/quarkmasses_fundamental")[0]
-    mas  = get_hdf5_value(f,ensemble+"/"+channel+"/quarkmasses_antisymmetric")[0]
-
-    out = open(outfile, "a")
-    outHR = open(outfileHR, "a")
-    out.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,mf,mas,beta,gv.mean(E1),gv.sdev(E1),gv.mean(E2),gv.sdev(E2),chi2dofA,chi2dofB))
-    outHR.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % (ensemble,channel,T,L,mf,mas,beta,E1,E2,chi2dofA,chi2dofB))
-    out.close()
-    outHR.close()
-
-def run_corrfitter_singlets(prmfile,hdf5path,outdir,resample=False):
-
+def run_corrfitter_singlets(prmfile,hdf5file,outdir):
     outfile    = os.path.join(outdir,"corrfitter_results.csv")
     outfileHR  = os.path.join(outdir,"corrfitter_results_HR.csv")
-    outfile2   = os.path.join(outdir,"corrfitter_results_jackknife.csv")
-    outfile2HR = os.path.join(outdir,"corrfitter_results_jackknife_HR.csv")
-    os.path.exists(outfile)     and os.remove(outfile)
-    os.path.exists(outfileHR)   and os.remove(outfileHR)
-    os.path.exists(outfile2)    and os.remove(outfile2)
-    os.path.exists(outfile2HR)  and os.remove(outfile2HR)
+    os.path.exists(outfile)   and os.remove(outfile)
+    os.path.exists(outfileHR) and os.remove(outfileHR)
     
-    hdf5file = os.path.join(hdf5path,"singlets_smeared_eigenvalues.hdf5")
 
     with open(prmfile) as csvfile:
         reader = csv.DictReader(csvfile,delimiter=';')
         for row in reader:
-            ensemble, channel = row['ensemble'], row['channel']
-            tmin1, tmin2 = int(row['tmin1']), int(row['tmin2'])
-            tmax1, tmax2 = int(row['tmax1']), int(int(row['tmax2']))
-            tp, Nmax = int(row['tp']), int(row['Nmax'])
+            ensemble, channel, rep = row['ensemble'], row['channel'], row["rep"]
+            tmin, tmax = int(row['tmin']), int(row['tmax'])
+            tp,   Nmax = int(row['tp']), int(row['Nmax'])
+            fit_eigenvalues(outfile,outfileHR,hdf5file,tmin,tmax,tp,Nmax,ensemble,channel,rep)
 
-            fit_eigenvalues(outfile,outfileHR,hdf5file,tmin1,tmin2,tmax1,tmax2,tp,Nmax,ensemble,channel)
-            if resample:
-                fit_eigenvalues_resample(outfile2,outfile2HR,hdf5file,tmin1,tmin2,tmax1,tmax2,tp,Nmax,ensemble,channel)
 
 PLOT=False
 PRINT=False
@@ -162,14 +96,8 @@ PRINT=False
 args = sys.argv
 if len(args) < 4:
     print("Missing parameter and/or hdf5 file")
-elif len(args)>=5:
+elif len(args)>=4:
     prmfile  = args[1]
     hdf5path = args[2] 
     outdir   = args[3] 
-    resample = args[4] == 'True'
-    run_corrfitter_singlets(prmfile,hdf5path,outdir,resample)
-elif len(args)==4:
-    prmfile  = args[1]
-    hdf5path = args[2] 
-    outdir   = args[3] 
-    run_corrfitter_singlets(prmfile,hdf5path,outdir,resample=False)
+    run_corrfitter_singlets(prmfile,hdf5path,outdir)
--- scripts/write_hdf5.jl
+++ scripts/write_hdf5.jl
@@ -1,20 +1,31 @@
 using DelimitedFiles
 using HiRepParsing
 
-function main_write_hdf5_logs(path,hdf5path,parameterfile)
-    h5file = joinpath(hdf5path,"b55_tests.hdf5")
+function main_write_hdf5_logs(path,h5file,parameterfile)
     input = readdlm(parameterfile,';',skipstart=1)
     for prm in eachrow(input)
-
+        
         dir, file, typeCONN, rep, name = prm
         fileCONN = joinpath(path,dir,"out",file)
-        
+
         @show fileCONN   
         writehdf5_spectrum(fileCONN,h5file,typeCONN,h5group="$name/$rep/CONN")
     end    
 end
 
 path = "/home/fabian/Dokumente/DataDiaL/measurementsTests"
-hdf5path = ""
+h5file = "/home/fabian/Dokumente/Physics/Analysis/HiRepHadrons/b55_tests.hdf5"
 parameterfile = "input/parameters.csv"
-main_write_hdf5_logs(path,hdf5path,parameterfile)
+main_write_hdf5_logs(path,h5file,parameterfile)
+
+function run_corrfitter(parameters,hdf5file,outdir)
+    args = `$(abspath(parameters)) $(abspath(hdf5file)) $(abspath(outdir))`
+    try
+        run(`python3 $(abspath("scripts/fitting_eigenvalues.py")) $args`)
+    catch
+        run(`python  $(abspath("scripts/fitting_eigenvalues.py")) $args`)
+    end
+end
+
+parameters_fitting = "input/parameters_fitting.csv"
+run_corrfitter(parameters_fitting,h5file,"output")
