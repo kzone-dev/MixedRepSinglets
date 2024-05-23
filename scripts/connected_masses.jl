3dff36351e5fb8f822b4d34badb9d074b4d9074a
Fabian Zierler <fabian.zierler@a1.net>
2024-05-23 15:49:54 +0100

    plot effective masses

--- scripts/connected_masses.jl
+++ scripts/connected_masses.jl
@@ -1,5 +1,9 @@
 using DelimitedFiles
 using HiRepParsing
+using MixedRepSinglets
+using HDF5
+using Plots
+gr(frame=:box)
 
 function main_write_hdf5_logs(path,h5file,parameterfile)
     input = readdlm(parameterfile,';',skipstart=1)
@@ -21,12 +25,12 @@ isfile(h5file) && rm(h5file)
 ispath("output") || mkpath("output")
 
 path = joinpath(datapath,"measurementsTests")
-parameterfile = "input/parameters_b6p55.csv"
-main_write_hdf5_logs(path,h5file,parameterfile)
+parameterfile655 = "input/parameters_b6p55.csv"
+main_write_hdf5_logs(path,h5file,parameterfile655)
 
 path = joinpath(datapath,"measurements")
-parameterfile = "input/parameters_b6p45.csv"
-main_write_hdf5_logs(path,h5file,parameterfile)
+parameterfile645 = "input/parameters_b6p45.csv"
+main_write_hdf5_logs(path,h5file,parameterfile645)
 
 function run_corrfitter(parameters,hdf5file,outdir)
     args = `$(abspath(parameters)) $(abspath(hdf5file)) $(abspath(outdir))`
@@ -39,3 +43,20 @@ end
 
 parameters_fitting = "input/parameters_fitting.csv"
 run_corrfitter(parameters_fitting,h5file,"output")
+
+results  = readdlm("output/corrfitter_results.csv",';')
+fitparam = readdlm("input/parameters_fitting.csv",';',skipstart=1)
+for (i,line) in enumerate(eachrow(results))
+    ens, channel, rep, T, L, β, m, Δm, χ2dof = line
+    tmin, tmax, tp, Nmax = fitparam[i,4:7]
+    
+    label = "$ens/$rep/CONN/DEFAULT_SEMWALL TRIPLET/$channel"
+    corr  = h5read(h5file,label)
+    meff, Δmeff = implicit_meff_jackknife(corr')
+    #meff = correlator_folding(meff;t_dim=1,sign=+1)
+
+    range = 1:div(T,2)
+    ylims = (m - 10Δm,m + 10Δm)
+    plt = scatter(meff[range],yerr=Δmeff[range];ylims,label="effective mass")
+    display(plt)
+end
