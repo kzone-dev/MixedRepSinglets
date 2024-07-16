e85de94125a56e6a2d372433716c841fe524d9d4
Fabian Zierler <fabian.zierler@a1.net>
2024-07-16 14:14:05 +0100

    tentative signal

--- scripts/single_rep_AS.jl
+++ scripts/single_rep_AS.jl
@@ -22,18 +22,33 @@ if write
     writehdf5_spectrum_disconnected_with_regexp(fileDISC,h5file,typesDISC,nhits,h5group="$name/DISC")
 end
 
-re
-conn, disc = _assemble_correlation_matrix_rep(h5file,name,Nsmear,"";channel="g5",disc_sign=+1,subtract_vev=false,Nf=3)
+assemble = false
+if assemble
+    conn, disc = _assemble_correlation_matrix_rep(h5file,name,Nsmear,"";channel="g5",disc_sign=+1,subtract_vev=false,Nf=3)
+end
 correlation_matrix = @. conn - disc
 
 using LaTeXStrings
-include("utils.jl")
-eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=true)
-
 using Plots
 plotlyjs()
-plt = plot()
-for i in 3:3
-    scatter!(plt,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,16))
-end
+pgfplotsx()
+pgfplotsx(legend=:topright, frame=:box, legendfontsize=14, tickfontsize=14, labelfontsize=14, titlefontsize=16,  markersize=5)
+
+include("utils.jl")
+plt = plot(frame=:box)
+
+
+i = 3
+range = 1:13
+eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=true)
+scatter!(plt,meff[i,range], yerr = Δmeff[i,range],ylims=(0,1),marker=:circle,label="singlet (GEVP)")
+eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(conn;t0=1,binsize=1,deriv=false)
+scatter!(plt,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,22.5),marker=:pent,label="non-singlet (GEVP)")
+
+correlation_matrix_deriv = conn
+corrN60N0 = correlation_matrix_deriv[1,3,:,:]
+meffN60N0, ΔmeffN60N0 = implicit_meff_jackknife(corrN60N0';sign=+1)
+#scatter!(plt,meffN60N0, yerr = ΔmeffN60N0,ylims=(0.2,0.6),xlims=(0,22.5),marker=:pent,label="non-singlet (N1=60, N2=0)")
+
+
 plt
