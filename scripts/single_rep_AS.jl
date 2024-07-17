b3c832e8352d88a390373873f249c9de7fb6b653
Fabian Zierler <fabian.zierler@a1.net>
2024-07-17 17:26:02 +0100

    simplified visual examination

--- scripts/single_rep_AS.jl
+++ scripts/single_rep_AS.jl
@@ -1,15 +1,20 @@
 using HiRepParsing
 using MixedRepSinglets
+using LaTeXStrings
+using Plots
+include("utils.jl")     
+#pgfplotsx(legend=:topright, frame=:box, legendfontsize=14, tickfontsize=14, labelfontsize=14, titlefontsize=16,  markersize=5)
+plotlyjs()
 
-path = "/home/fabian/Documents/Physics/Data/DataDiaL/measurementsAS"
-fileCONN = joinpath(path,"Lt56Ls24beta6.8mas-1.035/out/out_spectrum_smeared")
-fileDISC = joinpath(path,"Lt56Ls24beta6.8mas-1.035/out/out_spectrum_smeared_discon")
-name  = "AS1"
-nhits = 200
-h5file = "test_AS.hdf5"
+path = "/home/fabian/Documents/Physics/Data/DataDiaL/measurementsAS/Lt56Ls24beta6.8mas-1.035/out"
+path = "/home/fabian/Downloads/smearing_old_complex_numbers/"
+fileCONN = joinpath(path,"out_spectrum_smeared")
+fileDISC = joinpath(path,"out_spectrum_smeared_discon")
+name  = "F1"
+nhits = 128
+h5file = "test_F.hdf5"
 
-
-Nsmear = ("0","30","60")
+Nsmear = ("0","40","80")
 typesDISC = ["DISCON_SEMWALL smear_N$N SINGLET"  for N  in Nsmear]
 typesCONN = ["source_N$(N1)_sink_N$(N2) TRIPLET" for N1 in Nsmear, N2 in Nsmear]
 
@@ -25,48 +30,19 @@ end
 assemble = false
 if assemble
     conn, disc = _assemble_correlation_matrix_rep(h5file,name,Nsmear,"";channel="g5",disc_sign=+1,subtract_vev=false,Nf=3)
+    correlation_matrix = @. conn - disc
+    correlation_matrix_deriv = correlator_derivative(correlation_matrix,t_dim=4)
 end
-correlation_matrix = @. conn - disc
 
-using LaTeXStrings
-using Plots
-plotlyjs()
-pgfplotsx()
-pgfplotsx(legend=:topright, frame=:box, legendfontsize=14, tickfontsize=14, labelfontsize=14, titlefontsize=16,  markersize=5)
-
-include("utils.jl")     
-plt = plot(frame=:box)
-
-
-i = 3
-range = 1:13
-eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=false)
-scatter!(plt,meff[i,range], yerr = Δmeff[i,range],ylims=(0,1),marker=:circle,label="singlet (GEVP)")
-eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(conn;t0=1,binsize=1,deriv=false)
-scatter!(plt,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,22.5),marker=:pent,label="non-singlet (GEVP)")
-
-plotlyjs()
-plt3 = plot()
-correlation_matrix_deriv = correlator_derivative(conn - disc,t_dim=4)
+plt3 = plot(legend=:bottomright,legendfontsize=10)
 for ind in [(1,1),(3,1),(3,3)] 
-    corrN60N0 = correlation_matrix_deriv[ind[1],ind[2],:,:]
-    meffN60N0, ΔmeffN60N0 = implicit_meff_jackknife(corrN60N0';sign=+1)
-    scatter!(plt3,meffN60N0, yerr = ΔmeffN60N0,ylims=(0.2,0.6),xlims=(0,22.5),marker=:pent,label="non-singlet (N1=$(Nsmear[ind[1]]), N2=$(Nsmear[ind[2]]))")
+    corr = correlation_matrix_deriv[ind[1],ind[2],:,:]
+    meff, Δmeff = implicit_meff_jackknife(corr';sign=-1)
+    scatter!(plt3,meff, yerr = Δmeff, label="singlet (N1=$(Nsmear[ind[1]]), N2=$(Nsmear[ind[2]]))")
 end
+eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=false)
+scatter!(plt3,meff[3,:], yerr = Δmeff[3,:],label="singlet (GEVP)")
 eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(conn;t0=1,binsize=1,deriv=false)
-scatter!(plt3,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,22.5),marker=:pent,label="non-singlet (GEVP)")
+scatter!(plt3,meff[3,:], yerr = Δmeff[3,:],label="non-singlet (GEVP)")
+plot!(plt3,ylims=(0,1),xlims=(0,12.5))
 display(plt3)
-
-using Statistics
-plotlyjs()
-plt2 = plot()
-N = size(disc)[3]
-disc_avg = dropdims(mean(disc,dims=3),dims=3)
-disc_std = dropdims(std(disc,dims=3),dims=3)./sqrt(N)
-scatter!(plt2, disc_avg[1,1,:],yerr=disc_std[1,1,:])
-scatter!(plt2, disc_avg[2,2,:],yerr=disc_std[2,2,:])
-scatter!(plt2, disc_avg[3,3,:],yerr=disc_std[3,3,:])
-
-plt
-plt2
-2+2
