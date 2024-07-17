9a507e6ea8452e08e389cd36a427993282e3239a
Fabian Zierler <fabian.zierler@a1.net>
2024-07-17 16:29:00 +0100

    plot more correlators

--- scripts/single_rep_AS.jl
+++ scripts/single_rep_AS.jl
@@ -34,21 +34,39 @@ plotlyjs()
 pgfplotsx()
 pgfplotsx(legend=:topright, frame=:box, legendfontsize=14, tickfontsize=14, labelfontsize=14, titlefontsize=16,  markersize=5)
 
-include("utils.jl")
+include("utils.jl")     
 plt = plot(frame=:box)
 
 
 i = 3
 range = 1:13
-eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=true)
+eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(correlation_matrix;t0=1,binsize=1,deriv=false)
 scatter!(plt,meff[i,range], yerr = Δmeff[i,range],ylims=(0,1),marker=:circle,label="singlet (GEVP)")
 eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(conn;t0=1,binsize=1,deriv=false)
 scatter!(plt,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,22.5),marker=:pent,label="non-singlet (GEVP)")
 
-correlation_matrix_deriv = conn
-corrN60N0 = correlation_matrix_deriv[1,3,:,:]
-meffN60N0, ΔmeffN60N0 = implicit_meff_jackknife(corrN60N0';sign=+1)
-#scatter!(plt,meffN60N0, yerr = ΔmeffN60N0,ylims=(0.2,0.6),xlims=(0,22.5),marker=:pent,label="non-singlet (N1=60, N2=0)")
+plotlyjs()
+plt3 = plot()
+correlation_matrix_deriv = correlator_derivative(conn - disc,t_dim=4)
+for ind in [(1,1),(3,1),(3,3)] 
+    corrN60N0 = correlation_matrix_deriv[ind[1],ind[2],:,:]
+    meffN60N0, ΔmeffN60N0 = implicit_meff_jackknife(corrN60N0';sign=+1)
+    scatter!(plt3,meffN60N0, yerr = ΔmeffN60N0,ylims=(0.2,0.6),xlims=(0,22.5),marker=:pent,label="non-singlet (N1=$(Nsmear[ind[1]]), N2=$(Nsmear[ind[2]]))")
+end
+eigvals, Δeigvals, meff, Δmeff, eigenvalues_jackknife = eigenvalues_meff_mixed_rep(conn;t0=1,binsize=1,deriv=false)
+scatter!(plt3,meff[i,:], yerr = Δmeff[i,:],ylims=(0,1),xlims=(0,22.5),marker=:pent,label="non-singlet (GEVP)")
+display(plt3)
 
+using Statistics
+plotlyjs()
+plt2 = plot()
+N = size(disc)[3]
+disc_avg = dropdims(mean(disc,dims=3),dims=3)
+disc_std = dropdims(std(disc,dims=3),dims=3)./sqrt(N)
+scatter!(plt2, disc_avg[1,1,:],yerr=disc_std[1,1,:])
+scatter!(plt2, disc_avg[2,2,:],yerr=disc_std[2,2,:])
+scatter!(plt2, disc_avg[3,3,:],yerr=disc_std[3,3,:])
 
 plt
+plt2
+2+2
