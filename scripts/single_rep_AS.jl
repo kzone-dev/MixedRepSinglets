3d02239e8c0128162c2cff2038e79b6343f96e4e
Fabian Zierler <fabian.zierler@a1.net>
2024-07-25 23:16:45 +0100

    all plots

--- scripts/single_rep_AS.jl
+++ scripts/single_rep_AS.jl
@@ -4,8 +4,7 @@ using LaTeXStrings
 using Plots
 include("utils.jl")
 include("utils_single_rep.jl")
-function plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn)
-    deriv  = !scalar 
+function plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn,deriv)
     meff1, Δmeff1, meff2, Δmeff2, conn, correlation_matrix, correlation_matrix_deriv = correlation_matrices_single_rep(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,deriv)
     plt = plot_singlet_vs_nonsinglets(meff1, Δmeff1, meff2, Δmeff2, conn, correlation_matrix, correlation_matrix_deriv, Nsmear; xlim, Ns_conn, deriv, plot_sing, plot_conn)
     return plt
@@ -17,14 +16,39 @@ pgfplotsx(legendfonthalign = :left,legend=:topright, frame=:box, legendfontsize=
 path = "/home/fabian/Downloads/smearing_old_complex_numbers/"
 fileCONN = joinpath(path,"out_spectrum_smeared")
 fileDISC = joinpath(path,"out_spectrum_smeared_discon")
-name  = "F1"
-nhits = 128
-h5file = "test.hdf5"
-Nsmear = ("0","40")
-Nsmear = ("0","40","80")
 mπREF, ΔmπREF = 0.5663, 0.0008  
-mηREF, ΔmηREF = 0.6100, 0.0060  
+mVREF, ΔmVREF = 0.6994, 0.0018
+mηREF, ΔmηREF = 0.6100, 0.0060
+h5file = "test.hdf5"
+name   = "F1"
+write  = false
+nhits  = 128
 Nf = 2
+
+scalar  = false
+deriv   = false
+Nsmear  = ("0","40","80")
+Ns_conn = [(2,1)]
+xlim    = (0,12.5)
+plot_sing, plot_conn = true, false
+plt3 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn,deriv)
+plot!(plt3;xlims=xlim,ylims=(0.50,0.70),legend=:topleft)
+
+scalar  = true
+deriv   = false
+Nsmear  = ("0","40","80")
+Ns_conn = []
+xlim    = (0,6.5)
+plot_sing, plot_conn = true, false
+plt4 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn,deriv)
+plot!(plt4;xlims=xlim,ylims=(0.50,0.80),legend=:topleft)
+
+for plt in [plt3, plt4]
+    add_mass_band!(plt,mπREF,ΔmπREF;label="PS",alpha=0.5)
+    add_mass_band!(plt,mVREF,ΔmVREF;label="V",alpha=0.5)
+    add_mass_band!(plt,mηREF,ΔmηREF;label="η",alpha=0.5)
+    display(plt)
+end
 
 path = "/home/fabian/Documents/Physics/Data/DataDiaL/measurementsAS/Lt56Ls24beta6.8mas-1.035/out"
 path = "/home/fabian/Dokumente/Physics/Data/DataDiaL/measurementsAS/Lt56Ls24beta6.8mas-1.035/out"
@@ -47,19 +71,21 @@ fileDISC = joinpath(path,"out_spectrum_smeared_discon_N240")
 name     = "AS5"
 
 scalar  = false
+deriv   = !scalar 
 Nsmear  = collect(0:240:240)
 Ns_conn = [(2,1)]
 xlim    = (0,12.5)
 plot_sing, plot_conn = true, false
-plt1 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn)
+plt1 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn,deriv)
 plot!(plt1;xlims=xlim,ylims=(0.30,0.45),legend=:topleft)
 
 scalar  = true
+deriv   = !scalar 
 Nsmear  = (0,60,240)
 Ns_conn = []
 xlim    = (0,8.5)
 plot_sing, plot_conn = true, false
-plt2 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn)
+plt2 =  plot_from_logs(fileCONN,fileDISC,h5file,name,Nsmear,nhits;write,scalar,Nf,xlim,Ns_conn,plot_sing,plot_conn,deriv)
 plot!(plt2;xlims=xlim,ylims=(0.30,0.55),legend=:topleft)
 
 for plt in [plt1,plt2]
